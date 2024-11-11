d3967fc5bfde87d5fba061fce31c2512afac3eb0
Shrikar Nakhye <126857057+ItsShriks@users.noreply.github.com>
2024-11-11 23:51:42 +0100

    Update install_ubuntu.sh

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,4 +1,4 @@
-sudo ubuntu-drivers install
+sudo ubuntu-drivers install #NVIDIA GPUs Only
 sudo apt install terminator
 sudo apt install git
 sudo snap install code
@@ -8,3 +8,5 @@ rm -rf ~/Pictures/
 rm -rf ~/Public/
 rm -rf ~/Documents/
 rm -rf ~/Templates/
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
