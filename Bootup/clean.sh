84b4fa021a482977d28b0823d026521dec6db8b5
Shrikar Nakhye <nakhyeshrikar@icloud.com>
2025-07-28 00:17:46 +0200

    Done

--- Bootup/clean.sh
+++ Bootup/clean.sh
@@ -1,11 +1,25 @@
-echo -e "\n>>> Running: Homebrew Clean "
+#!/bin/bash
+
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+SEPARATOR="${BLUE}------------------------------------------------------------${NC}"
+
+echo -e "\n$SEPARATOR"
+echo -e "${BLUE}>>> Running: Homebrew Clean${NC}"
+echo -e "$SEPARATOR"
 brew cleanup -s
 brew cleanup
-echo "Brew Cleanup completed"
+echo -e "${GREEN}Brew Cleanup completed${NC}"
 
-echo -e "\n>>> Autoremoving brew"
+echo -e "\n$SEPARATOR"
+echo -e "${BLUE}>>> Autoremoving Brew${NC}"
+echo -e "$SEPARATOR"
 brew autoremove
-echo "Brew Autoremove completed"
+echo -e "${GREEN}Brew Autoremove completed${NC}"
 
-echo -e "\n>>> Running: conda clean --all -y"
+echo -e "\n$SEPARATOR"
+echo -e "${BLUE}>>> Running: conda clean --all -y${NC}"
+echo -e "$SEPARATOR"
 conda clean --all -y
+echo -e "${GREEN}Conda Cleanup completed${NC}"
