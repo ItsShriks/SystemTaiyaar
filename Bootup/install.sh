d9b73b6e1490f8a52837d8e4d0ce2d6510a7d199
Shrikar Nakhye <nakhyeshrikar@icloud.com>
2025-07-30 01:31:22 +0200

    Add Apple iWork

--- Bootup/install.sh
+++ Bootup/install.sh
@@ -14,20 +14,27 @@ brew install --cask discord
 brew install --cask docker
 brew install --cask epic-games
 brew install --cask google-chrome
+brew install --cask google-drive
 brew install --cask grammarly
+brew install --cask keynote
 brew install --cask logi-options-plus
+brew install mac-mouse-fix
+brew install mas
 brew install --cask mattermost
 brew install --cask microsoft-excel
 brew install --cask microsoft-powerpoint
 brew install --cask microsoft-word
 brew install --cask miniconda
 brew install --cask notion
+brew install --cask numbers
+brew install --cask pages
 brew install --cask pycharm
 brew install --cask raspberry-pi-imager
 brew install --cask slack
 brew install --cask steam
 brew install --cask telegram
 brew install --cask visual-studio-code
+brew install --cask vlc
 brew install --cask vmware-fusion
 brew install --cask vnc-viewer
 brew install --cask webex
@@ -35,6 +42,10 @@ brew install --cask whatsapp
 brew install --cask zed
 brew install --cask zoom
 brew install --cask zotero
+
+mas install 409203825 # Numbers
+mas install 409201541 # Pages
+mas install 409183694 # Keynote
 
 brew autoremove
 brew cleanup
